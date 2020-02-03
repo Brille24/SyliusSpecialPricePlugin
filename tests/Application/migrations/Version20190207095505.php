c461e5ffa495542ab05c46e4e714fe9367892f8a
Peter Ukena <peter.ukena@brille24.de>
2020-02-03 14:00:11 +0100

    Removed unneccesary SQL statement from migration
    This line has nothing to do with this plugin and is probably from a faulty database state.

--- tests/Application/migrations/Version20190207095505.php
+++ tests/Application/migrations/Version20190207095505.php
@@ -17,7 +17,6 @@ final class Version20190207095505 extends AbstractMigration
 
         $this->addSql('CREATE TABLE brille24_channel_special_pricing (id INT AUTO_INCREMENT NOT NULL, product_variant_id INT NOT NULL, price INT NOT NULL, channelCode VARCHAR(255) NOT NULL, startsAt DATETIME NOT NULL, endsAt DATETIME NOT NULL, INDEX IDX_1FF2651CA80EF684 (product_variant_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE brille24_channel_special_pricing ADD CONSTRAINT FK_1FF2651CA80EF684 FOREIGN KEY (product_variant_id) REFERENCES sylius_product_variant (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE sylius_channel DROP taxId');
     }
 
     public function down(Schema $schema) : void
@@ -26,6 +25,5 @@ final class Version20190207095505 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE brille24_channel_special_pricing');
-        $this->addSql('ALTER TABLE sylius_channel ADD taxId VARCHAR(255) DEFAULT NULL COLLATE utf8_unicode_ci');
     }
 }

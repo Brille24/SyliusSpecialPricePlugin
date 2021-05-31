86d297f316a1455acb494a3e4bf5ac3d76f1c62c
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2021-05-31 11:51:25 +0200

    Added a method to check if the special price is active for a given date

--- src/Entity/ChannelSpecialPricing.php
+++ src/Entity/ChannelSpecialPricing.php
@@ -38,91 +38,88 @@ class ChannelSpecialPricing implements ChannelSpecialPricingInterface
      */
     protected $endsAt;
 
-    /**
-     * {@inheritdoc}
-     */
     public function getId()
     {
         return $this->id;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getProductVariant(): ?ProductVariantInterface
     {
         return $this->productVariant;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function setProductVariant(?ProductVariantInterface $productVariant): void
     {
         $this->productVariant = $productVariant;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getPrice(): ?int
     {
         return $this->price;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function setPrice(?int $price): void
     {
         $this->price = $price;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getChannelCode(): ?string
     {
         return $this->channelCode;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function setChannelCode(?string $channelCode): void
     {
         $this->channelCode = $channelCode;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getStartsAt(): ?\DateTimeInterface
     {
         return $this->startsAt;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function setStartsAt(?\DateTimeInterface $startsAt): void
     {
         $this->startsAt = $startsAt;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getEndsAt(): ?\DateTimeInterface
     {
         return $this->endsAt;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function setEndsAt(?\DateTimeInterface $endsAt): void
     {
         $this->endsAt = $endsAt;
+    }
+
+    public function isActive(?\DateTimeInterface $dateTime): bool
+    {
+        if (null === $dateTime) {
+            $dateTime = new \DateTime('now');
+        }
+
+        // Account for null start and end
+        if ($this->startsAt === null && $this->endsAt === null) {
+            return true;
+        }
+
+        // Account for null start
+        if ($this->startsAt === null && $dateTime < $this->endsAt) {
+            return true;
+        }
+
+        // Account for null end
+        if ($this->endsAt === null && $dateTime >= $this->startsAt) {
+            return true;
+        }
+
+        // Check if $dateTime is between start and end date of the pricing.
+        if ($dateTime >= $this->startsAt &&
+            $dateTime < $this->endsAt) {
+            return true;
+        }
+
+        return false;
     }
 }
--- src/Entity/ChannelSpecialPricingInterface.php
+++ src/Entity/ChannelSpecialPricingInterface.php
@@ -9,53 +9,25 @@ use Sylius\Component\Resource\Model\ResourceInterface;
 
 interface ChannelSpecialPricingInterface extends ResourceInterface
 {
-    /**
-     * @return int|null
-     */
     public function getPrice(): ?int;
 
-    /**
-     * @param int|null $price
-     */
     public function setPrice(?int $price): void;
 
-    /**
-     * @return ProductVariantInterface|null
-     */
     public function getProductVariant(): ?ProductVariantInterface;
 
-    /**
-     * @param ProductVariantInterface|null $productVariant
-     */
     public function setProductVariant(?ProductVariantInterface $productVariant): void;
 
-    /**
-     * @return string|null
-     */
     public function getChannelCode(): ?string;
 
-    /**
-     * @param string|null $channelCode
-     */
     public function setChannelCode(?string $channelCode): void;
 
-    /**
-     * @return \DateTimeInterface|null
-     */
     public function getStartsAt(): ?\DateTimeInterface;
 
-    /**
-     * @param \DateTimeInterface|null $startsAt
-     */
     public function setStartsAt(?\DateTimeInterface $startsAt): void;
 
-    /**
-     * @return \DateTimeInterface|null
-     */
     public function getEndsAt(): ?\DateTimeInterface;
 
-    /**
-     * @param \DateTimeInterface|null $endsAt
-     */
     public function setEndsAt(?\DateTimeInterface $endsAt): void;
+
+    public function isActive(?\DateTimeInterface $dateTime): bool;
 }
